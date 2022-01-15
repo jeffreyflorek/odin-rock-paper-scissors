c32ca6d4ec9b3cb8404ce68eeb3afe4729ced98d
Jeff Florek <jkflorek@gmail.com>
2022-01-15 17:59:42 -0500

    Add basic UI for the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -59,7 +59,7 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
+/*function game() {
   let playerScore = 0;
   let computerScore = 0;
 
@@ -91,5 +91,59 @@ function game() {
     alert(`It's a tie! You both scored ${playerScore}`);
   }
 
+}*/
 
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  resultsDiv.innerHTML = '';
+  scoreDiv.innerHTML = '';
 }
+
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+const resultsDiv = document.querySelector('#results');
+const scoreDiv = document.querySelector('#score');
+
+buttons.forEach(button => button.addEventListener('click', event => {
+  const playerSelection = event.target.id[0].toUpperCase() + event.target.id.substring(1).toLowerCase();;
+  const computerSelection = computerPlay();
+  const result = playRound(playerSelection, computerSelection);
+
+  switch (result) {
+    case 'win':
+      resultsDiv.innerHTML = `
+        <h2>You win this round!</h2>
+        <p>${playerSelection} beats ${computerSelection.toLowerCase()}.</p>
+      `;
+      playerScore++;
+      break;
+    case 'lose':
+      resultsDiv.innerHTML = `
+        <h2>You've lost this round!</h2>
+        <p>${computerSelection} beats ${playerSelection.toLowerCase()}.</p>
+      `;
+      computerScore++
+      break;
+    case 'tie':
+      resultsDiv.innerHTML = `
+        <h2>It's a tie!</h2>
+        <p>We both played ${playerSelection.toLowerCase()}.</p>
+      `;
+  }
+
+  scoreDiv.innerHTML = `
+    <h2>Current Scores</h2>
+    <p>Player: ${playerScore}</p>
+    <p>Computer: ${computerScore}</p>
+  `;
+
+  if (playerScore >= 5) {
+    alert(`You've won the game!\nFinal score: ${playerScore} to ${computerScore}.`);
+    resetGame();
+  } else if (computerScore >= 5) {
+    alert(`You've lost the game!\nFinal score: ${computerScore} to ${playerScore}.`);
+    resetGame();
+  }
+}));
